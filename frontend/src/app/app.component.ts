08a956356a15822ddb6468a55fe03c3025106f6d
Horacio Cortez <hcortez@arquant.com.ar>
2025-03-24 08:15:57 -0300

    testing

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,65 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { TableModule } from 'primeng/table';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { DropdownModule } from 'primeng/dropdown';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  providers: [HttpClient],
+  imports: [
+    CommonModule,
+    FormsModule,
+    TableModule,
+    InputTextModule,
+    ButtonModule,
+    CardModule,
+    DropdownModule,
+  ],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  productos: any[] = [];
+  nuevoProducto = {
+    producto: '',
+    presentacion: '',
+    stock: 0,
+    precio: 0,
+    vencimiento: '',
+  };
+  venta = { producto_id: 0, cantidad: 0 };
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.cargarProductos();
+  }
+
+  cargarProductos() {
+    this.http
+      .get<any[]>('http://localhost:8080/productos')
+      .subscribe((data) => {
+        this.productos = data;
+      });
+  }
+
+  agregarProducto() {
+    this.http
+      .post('http://localhost:8080/productos', this.nuevoProducto)
+      .subscribe(() => {
+        this.cargarProductos();
+      });
+  }
+
+  venderProducto() {
+    this.http.post('http://localhost:8080/vender', this.venta).subscribe(() => {
+      this.cargarProductos();
+    });
+  }
+}
